3bd5101cec8e872f3520592f2b2743440ecdfef5
AbacoDev <pedrofiliperibeiro@gmail.com>
2024-04-11 22:09:18 -0300

    terceiro commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,7 +10,42 @@ sap.ui.define([
         return Controller.extend("zcursopfrapp1.controller.View1", {
            onInit: function () {
 
+            },
+
+            onCreate: function(){
+                var that = this;
+                var usuario = this.getView().byId("idusuario").getValue();
+                var nome = this.getView().byId("idnome").getValue();
+                var proj = this.getView().byId("idproj").getValue();
+                var email = this.getView().byId("idemail").getValue();
+
+                if(!usuario){
+
+                    sap.m.MessageBox.error(this.getView().getModel("i18n").getResourceBundle().getText("lblMsgErroUser"));
+                    return;
+                }
+
+                var oDados = {
+                    Usuario: usuario,
+                    Nome: nome,
+                    ProjetoSegw: proj,
+                    Email: email
+
+                }
+                this.getView().getModel().create('/AlunoFioriSet', oDados, {
+                    success: function(oData, oResponse){
+                        sap.m.MessageBox.success(that.getView().getModel("i18n").getResourceBundle().getText("lblMsgCreateOK"))
+                        that.getView().byId("idusuario").setValue("");
+                        that.getView().byId("idnome").setValue("");
+                        that.getView().byId("idproj").setValue("");
+                        that.getView().byId("idemail").setValue("");
+                    },
+                    error: function(oError){
+                        sap.m.MessageBox.success(that.getView().getModel("i18n").getResourceBundle().getText("lblMsgCreateError"))
+                    }
+                } )
             }
+
         });
         
     });
